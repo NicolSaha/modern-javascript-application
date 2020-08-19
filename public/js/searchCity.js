cd3e0f9305a6af1f82b02731c164de279b8939ed
“[Nicol <[nicol.saha@gmail.com]>
2020-08-19 19:47:49 +0200

    Adjusted Margins

--- public/js/searchCity.js
+++ public/js/searchCity.js
@@ -11,4 +11,30 @@
   }).configure({
     type: "city",
   });
+
+  let placesAutocompleteAddedOne = places({
+    appId: "plOSV629H4PZ",
+    apiKey: "f91689ee3fdf5af8eaf00eb81fdca94c",
+    container: document.querySelector("#search-city-input-added-city-one"),
+    templates: {
+      value: function (suggestion) {
+        return `${suggestion.name}`;
+      },
+    },
+  }).configure({
+    type: "city",
+  });
+
+  let placesAutocompleteAddedTwo = places({
+    appId: "plOSV629H4PZ",
+    apiKey: "f91689ee3fdf5af8eaf00eb81fdca94c",
+    container: document.querySelector("#search-city-input-added-city-two"),
+    templates: {
+      value: function (suggestion) {
+        return `${suggestion.name}`;
+      },
+    },
+  }).configure({
+    type: "city",
+  });
 })();
